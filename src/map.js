a41b690c1cb69102a2fa0e59a07d34a794a94334
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-08-11 03:02:09 -0300

    implementa método map com seus devidos testes

--- src/map.js
+++ src/map.js
@@ -1,1 +1,10 @@
-export default () => {}
+
+const map = (arr, func) => {
+    let newArr = []
+    for(let i = 0 ; i < arr.length; i++){
+        newArr.push(func(arr[i], i, arr))
+    }
+    return newArr
+}
+
+export default map
